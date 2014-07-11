1cb6a0c76237d06244eb0de0ca931222cb326690
Will Norris <will@willnorris.com>
2014-07-11 08:51:39 -0700

    new plugin: rewrite img urls to use imageproxy

---
+++ src/_plugins/imageproxy.rb
@@ -0,0 +1,28 @@
+# encoding: utf-8
+require 'liquid'
+require 'nokogiri'
+
+module Jekyll
+  class ImageProxyTag < Liquid::Tag
+    def render(context)
+      config = context.registers[:site].config
+      return context['content'] unless config.has_key?('url') and config.has_key?('imageproxy_baseurl')
+
+      page_url = URI.join(config['url'], context['page']['url'])
+
+      doc = Nokogiri::HTML.fragment(context['content'])
+      doc.search('.//img').each do |i|
+        img_url = URI.join(page_url, i.attributes['src'])
+        width = i.attributes['width']
+        height = i.attributes['height']
+        if "#{img_url}".start_with?(config['url']) and (width or height)
+          proxy_url = config['imageproxy_baseurl'] + "/#{width}x#{height}/#{img_url}"
+          i.attributes['src'].value = proxy_url
+        end
+      end
+      doc.inner_html
+    end
+  end
+end
+
+Liquid::Template.register_tag('proxied_content', Jekyll::ImageProxyTag)

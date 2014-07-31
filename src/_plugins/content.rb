191c395a1ea3e6252a498cf5101d639c7562a8cb
Will Norris <will@willnorris.com>
2014-07-31 14:06:55 -0700

    ensure content directories are matched properly

--- src/_plugins/content.rb
+++ src/_plugins/content.rb
@@ -16,7 +16,7 @@
 module Jekyll
   class StaticFile
     def destination(dest)
-      dir = @dir.sub(/^\/content\/([0-9]{4})/, '/\1')
+      dir = @dir.sub(/^\/content\/([0-9]{4}\/)/, '/\1')
       File.join(*[dest, dir, @name].compact)
     end
   end

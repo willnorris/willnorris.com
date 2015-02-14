27ddf6afe7ac267c96ba6672e61c93916edc1286
Will Norris <will@willnorris.com>
2015-02-13 23:53:19 -0800

    rename is_draft to draft?

--- src/_plugins/post_tools.rb
+++ src/_plugins/post_tools.rb
@@ -9,12 +9,12 @@ require 'nokogiri'
 module Jekyll
   class Post
     # Returns true if the current post is a draft.
-    def is_draft
-      self.kind_of? Jekyll::Draft
+    def draft?
+      self.is_a?(Jekyll::Draft)
     end
 
     def to_liquid_with_draft(attrs = ATTRIBUTES_FOR_LIQUID)
-      to_liquid_without_draft(attrs + %w[is_draft])
+      to_liquid_without_draft(attrs + %w[draft?])
     end
     alias_method :to_liquid_without_draft, :to_liquid
     alias_method :to_liquid, :to_liquid_with_draft

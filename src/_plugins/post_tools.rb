288ac8ba794faf7b204755eaeefdaa4e78548670
Will Norris <will@willnorris.com>
2015-02-20 13:30:47 -0800

    remove 'draft?' method; it's now in jekyll core

--- src/_plugins/post_tools.rb
+++ src/_plugins/post_tools.rb
@@ -7,19 +7,6 @@ require 'liquid'
 require 'nokogiri'
 
 module Jekyll
-  class Post
-    # Returns true if the current post is a draft.
-    def draft?
-      self.is_a?(Jekyll::Draft)
-    end
-
-    def to_liquid_with_draft(attrs = ATTRIBUTES_FOR_LIQUID)
-      to_liquid_without_draft(attrs + %w[draft?])
-    end
-    alias_method :to_liquid_without_draft, :to_liquid
-    alias_method :to_liquid, :to_liquid_with_draft
-  end
-
   module PostToolsFilters
     # Strip HTML that matches selector from content.  For example, to output the
     # post content stripped of any <aside> tags, use:

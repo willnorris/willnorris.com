f71cd8f0a35c8a8065eee1550e9f68c9f2740956
Will Norris <will@willnorris.com>
2022-12-11 08:49:33 -0800

    slides: remove io-slides submodule
    Add relevant io-slides files directly to wcsf2013 presentation, since
    it will be the only one to ever use it.

--- static/slides/wcsf2013/js/slides.js
+++ static/slides/wcsf2013/js/slides.js
@@ -1,10 +1,3 @@
-require([
-      '../../io-slides/js/order!../slide_config',
-      '../../io-slides/js/order!../../io-slides/js/modernizr.custom.45394',
-      '../../io-slides/js/order!../../io-slides/js/prettify/prettify',
-      '../../io-slides/js/order!../../io-slides/js/hammer',
-      '../../io-slides/js/order!../../io-slides/js/slide-controller',
-      '../../io-slides/js/order!../../io-slides/js/slide-deck'
-    ],
-    function(someModule) {}
-);
+require(['order!../slide_config', 'order!modernizr.custom.45394',
+         'order!hammer', 'order!slide-controller', 'order!slide-deck'],
+        function(someModule) {});

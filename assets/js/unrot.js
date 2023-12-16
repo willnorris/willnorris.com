b20b99158b619eb4f3be5fb4eb0a45f5cb148220
Will Norris <will@tailscale.com>
2023-12-15 22:38:33 -0800

    unrot: apply prettier formatting

--- assets/js/unrot.js
+++ assets/js/unrot.js
@@ -6,20 +6,19 @@
  * visit https://unrot.link
  */
 if (globalThis.fetch) {
-  //const root = 'https://unrot.link';
-  const root = '/api/unrot';
+  const root = "/api/unrot";
 
   // check if unrot.link is up using the ping service. It'll return a 206 (empty)
   // if it's up, or throw if it's down. The /ping endpoint is also cached for 1
   // day against the CDN
-  fetch(root + '/ping', {
-    method: 'HEAD',
-    mode: 'cors',
+  fetch(root + "/ping", {
+    method: "HEAD",
+    mode: "cors",
   })
     .then((res) => {
       if (res.status === 204) {
         document.body.addEventListener(
-          'click',
+          "click",
           (event) => {
             // use event delegation to only listen on clicks on links, then
             // filter out relative links and archive.org links so we're
@@ -27,18 +26,18 @@ if (globalThis.fetch) {
             // use the published date.
 
             /** @type HTMLElement */
-            const target = event.target.closest('a[href]');
+            const target = event.target.closest("a[href]");
 
             if (target) {
               /** @type String */
               let href = target.attributes.href.value;
 
-              if (!href.startsWith('http')) {
+              if (!href.startsWith("http")) {
                 // ignore relative urls
                 return;
               }
 
-              if (href.includes('archive.org')) {
+              if (href.includes("archive.org")) {
                 // don't try to redirect archive links
                 return;
               }
@@ -55,11 +54,11 @@ if (globalThis.fetch) {
 
               // now check if we have an h-entry, and if we do, try to use the
               // published date
-              const hEntry = target.closest('.h-entry');
+              const hEntry = target.closest(".h-entry");
 
               // if we have an h-entry with a published date, use that
               if (hEntry) {
-                const date = hEntry.querySelector('.dt-published[datetime]');
+                const date = hEntry.querySelector(".dt-published[datetime]");
                 if (date) {
                   target.href += `&date=${date.dateTime}`;
                 }
@@ -68,20 +67,20 @@ if (globalThis.fetch) {
           },
           {
             passive: true,
-          }
+          },
         );
       } else if (res.status === 402) {
         // note - this status is currently unused, but may be added in the future
         console.log(
-          "unrot.link does not have your site in the allow list. Please visit https://unrot.link/access/ to request access (it's free)."
+          "unrot.link does not have your site in the allow list. Please visit https://unrot.link/access/ to request access (it's free).",
         );
       } else {
         console.log(
-          'unrot.link is not responding correctly - possibly remove this script'
+          "unrot.link is not responding correctly - possibly remove this script",
         );
       }
     })
     .catch((_) => {
-      console.log('unrot.link is unreachable - possibly remove this script');
+      console.log("unrot.link is unreachable - possibly remove this script");
     });
 }

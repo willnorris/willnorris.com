e8a07f07468390ceebc61206b26d8e3fad97a56c
Will Norris <will@willnorris.com>
2023-12-28 02:27:25 -0800

    js: move popover into separate js file

---
+++ assets/js/popover.js
@@ -0,0 +1,30 @@
+const imageAnchors = Array.from(document.querySelectorAll(".e-content a"))
+  .filter((a) => a.href.match(/.(jpg|jpeg|png|webp)$/))
+  .filter((a) => a.querySelector("img"));
+if (imageAnchors.length) {
+  const popover = document.createElement("div");
+  popover.setAttribute("popover", "");
+  const img = document.createElement("img");
+  popover.appendChild(img);
+  document.body.append(popover);
+
+  for (const a of imageAnchors) {
+    a.addEventListener("click", (event) => {
+      event.preventDefault();
+      img.src = ""; // prevent flash of previous image
+      img.src = a.href;
+      console.log(popover);
+      popover.showPopover();
+    });
+  }
+
+  let lastScrollY = window.scrollY;
+  window.addEventListener("scroll", (event) => {
+    const deltaS = window.scrollY - lastScrollY;
+    lastScrollY = window.scrollY;
+    // close popover if scrolled more than 10px
+    if (Math.abs(deltaS) > 10) {
+      popover.hidePopover();
+    }
+  });
+}

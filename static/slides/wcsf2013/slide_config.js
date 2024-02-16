1d5127564b69258ee190d536ef5a309a44805704
Will Norris <will@willnorris.com>
2024-02-16 13:43:31 -0800

    slides/wcsf2013: remove web fonts and favicon

--- static/slides/wcsf2013/slide_config.js
+++ static/slides/wcsf2013/slide_config.js
@@ -9,11 +9,6 @@ var SLIDE_CONFIG = {
     usePrettify: false, // Default: true
     enableSlideAreas: false, // Default: true. False turns off the click areas on either slide of the slides.
     enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
-    favIcon: 'images/google_developers_logo_tiny.png',
-    fonts: [
-      'Open Sans:regular,semibold,italic,italicsemibold',
-      'Source Code Pro'
-    ],
     //theme: ['mytheme'], // Add your own custom themes or styles in /theme/css. Leave off the .css extension.
   },
 
